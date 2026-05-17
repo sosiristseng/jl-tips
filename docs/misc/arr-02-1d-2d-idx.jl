c004484d6f934578cbe05960f07b33373fff891f
Wen Wei Tseng <sosiristseng@gmail.com>
2026-05-17 21:42:36 +0800

    fix title

--- docs/misc/arr-02-1d-2d-idx.jl
+++ docs/misc/arr-02-1d-2d-idx.jl
@@ -1,8 +1,9 @@
-# Convert a linear index to 2D indices
+# # Convert a linear index to 2D indices
 # Use `CartesianIndices((nrow, ncol))`: See https://discourse.julialang.org/t/julia-usage-how-to-get-2d-indexes-from-1d-index-when-accessing-a-2d-array/61440
 x = rand((7, 10))
 CI = CartesianIndices((7, 10))
-for i in 1:length(x)
+
+for i in eachindex(x)
     r = CI[i][1]
     c = CI[i][2]
     @assert x[i] == x[r, c]
